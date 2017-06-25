3466852e4788b69920e3047b26b759d54c609270
Peter Kuehne <peter.kuehne@gmail.com>
2017-06-25 16:08:12 +0100

    Added alarm automation

--- appdaemon/apps/alarm.py
+++ appdaemon/apps/alarm.py
@@ -16,13 +16,24 @@ class AlarmController(api.AppDaemon):
         self.listen_state(self.alarm_trigger, "group.motion_detectors", new = "on")
         #self.listen_state(self.alarm_trigger, "group.door_sensors", new = "on")
 
-    def alarm_trigger(self, entity, attribute, old, new, kwargs):
+    def alarm_trigger(self, trigger_group, attribute, old, new, kwargs):
         self.log("A change was detected")
+        triggered_sensor = None
+        for sensor in self.get_state(trigger_group, "all")["attributes"]["entity_id"]:
+            if self.get_state(sensor) == "on":
+                # This one triggered the alarm
+                triggered_sensor = sensor
+                break
+
         if self.get_state("alarm_control_panel.ha_alarm") != "disarmed":
             self.log("Triggering the alarm")
-            self.notify("Entity {} triggered the alarm".format(
-                entity if entity is not None else "<Unknown>"),
+            # This could be moved to the trigger-reaction code
+            if triggered_sensor is not None:
+                sensor_name = self.get_state(triggered_sensor, "friendly_name")
+            else:
+                sensor_name = "Unknown"
+            self.notify("Entity <{}> triggered the alarm".format(sensor_name),
                 title="Home Assistant")
             self.call_service ("alarm_control_panel/alarm_trigger",
-                    entity_id = entity)
+                    entity_id = triggered_sensor)
 
